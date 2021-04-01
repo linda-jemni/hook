4f6ab37f11360775977403dbdf2df113db2e1990
linda <linda@gmail.com>
2021-04-01 15:51:43 +0200

    helloword

--- hello.py
+++ hello.py
@@ -3,10 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
+    """This function does blah blah."""
     return "Hello world !"
 ## question B.1.ii ###
 @app.route('/hello/linda')
 def optional():
+    """This function returns Hello <name>"""
     return "Hello "
+
 if __name__ == "__main__":
     app.run()
--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-echo "linda"
+print "linda"
