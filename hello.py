90e010d70311e9d0b9e6aa3c8172551a39e1410e
linda <linda@gmail.com>
2021-04-01 14:52:44 +0200

    hook

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
+## question B.1 .i ###
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-return "Hello world !"
+    return "Hello world !"
+## question B.1.ii ###
 @app.route('/hello/linda')
 def optional():
-return "Hello "
+    return "Hello "
 if __name__ == "__main__":
- app.run()
+    app.run()
